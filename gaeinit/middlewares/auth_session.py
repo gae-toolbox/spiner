7f8b212873b03ad628992c760c3deb6ab8a2c35a
Roman Nowicki <roman.nowicki@inbox.com>
2016-08-22 13:11:24 +0200

    pass kwargs to method wrapped by auth_session

--- gaeinit/middlewares/auth_session.py
+++ gaeinit/middlewares/auth_session.py
@@ -14,7 +14,7 @@ def auth_session(func):
         session = store.get_session()
 
         if not session:
-            self.redirect(uri_for('signin'))
+            self.redirect(uri_for('signin', **kwargs))
             return
         return func(self, session=session, *args, **kwargs)
     return func_wrapper
