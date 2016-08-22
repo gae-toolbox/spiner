371490384d1a1da267342915b6ff431877ef0c8f
Roman Nowicki <roman.nowicki@inbox.com>
2016-08-22 10:46:10 +0200

    new middleware: auth_session

---
+++ gaeinit/middlewares/auth_session.py
@@ -0,0 +1,20 @@
+from gaeinit import config
+from webapp2 import uri_for
+from webapp2_extras import sessions
+
+SECRET_KEY = config.getenv('SESSION_SECRET_KEY')
+EXPIRE = config.getenv('SESSION_EXPIRE')
+
+
+def auth_session(func):
+    """Auth request only when session is open and not expired"""
+    def func_wrapper(self, *args, **kwargs):
+        store = sessions.get_store(request=self.request)
+
+        session = store.get_session()
+
+        if not session:
+            self.redirect(uri_for('signin'))
+            return
+        return func(self, session=session, *args, **kwargs)
+    return func_wrapper
