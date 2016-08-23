5f1cbe2cf301f73d44c4e0d7beb3565a3785ef18
Roman Nowicki <roman.nowicki@inbox.com>
2016-08-23 11:38:22 +0200

    add passwd module

---
+++ gaeinit/passwd.py
@@ -0,0 +1,11 @@
+import bcrypt
+from gaeinit.config import getenv
+
+BCRYPT_ROUNDS = int(getenv('BCRYPT_ROUNDS'))
+
+
+def hash(pwd, salt=None):
+    """Returns encrypted password"""
+    if not salt:
+        salt = bcrypt.gensalt(BCRYPT_ROUNDS)
+    return bcrypt.hashpw(pwd, salt)
