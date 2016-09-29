953b10a0b98fc304bb04a8d5a4c60cef72fcf729
Roman Nowicki <roman.nowicki@inbox.com>
2016-09-29 12:11:52 +0200

    modify proxy to not initiate TIMEOUT variable during import

--- gaeinit/handlers/proxy.py
+++ gaeinit/handlers/proxy.py
@@ -1,8 +1,7 @@
+# -*- coding: utf-8 -*-
 from gaeinit.config import getenv
 from google.appengine.api import urlfetch
 from webapp2 import RequestHandler
-
-TIMEOUT = getenv('GAEINIT_HTTPPROXY_TIMEOUT')
 
 
 class Handler(RequestHandler):
@@ -23,7 +22,7 @@ class Handler(RequestHandler):
                 400,
                 "'url' parameter is mandatory, ...?url=http://example.com")
         self._build_response(urlfetch.fetch(
-            deadline=TIMEOUT,
+            deadline=_get_timeout(),
             follow_redirects=False,
             url=url))
 
@@ -31,7 +30,7 @@ class Handler(RequestHandler):
         url = self.request.get('url')
         self._build_response(urlfetch.fetch(
             method=urlfetch.PUT,
-            deadline=TIMEOUT,
+            deadline=_get_timeout(),
             follow_redirects=False,
             payload=self.request.body,
             url=url))
@@ -40,7 +39,7 @@ class Handler(RequestHandler):
         url = self.request.get('url')
         self._build_response(urlfetch.fetch(
             method=urlfetch.POST,
-            deadline=TIMEOUT,
+            deadline=_get_timeout(),
             follow_redirects=False,
             payload=self.request.body,
             url=url))
@@ -50,3 +49,7 @@ class Handler(RequestHandler):
         self.response.status = response.status_code
         self.response.headers = response.headers
         self.response.headers['X-Proxy'] = 'YES'
+
+
+def _get_timeout():
+    return getenv('GAEINIT_HTTPPROXY_TIMEOUT')
