ab8e80e263219dba7fdd17dd30301396331ca4ff
Roman Nowicki <rnowicki@Romans-MBP.home>
2017-11-14 10:57:36 +0100

    add _send method to jsonapi handler to standarise json response

--- spiner/handlers/jsonapi.py
+++ spiner/handlers/jsonapi.py
@@ -2,6 +2,7 @@
 
 import webapp2
 import json
+import spiner.config
 
 
 class Error(Exception):
@@ -21,6 +22,26 @@ class Handler(webapp2.RequestHandler):
             return json.loads(self.request.body)
         except ValueError:
             raise InvalidJsonInRequestBody('Malformed JSON in request body')
+
+    def _send(self, data, code=200, doc=None):
+        intent = None
+        separators = None
+        if spiner.config.is_debug_mode():
+            intent = 4
+            separators = (',', ': ')
+        self.response.content_type = 'application/json'
+        self.response.status = code
+        res = {
+            'status': 'SUCCESS',
+            'status_code': code,
+            'data': data
+            }
+
+        if doc:
+            res['doc'] = doc
+
+        self.response.write(json.dumps(
+            res, sort_keys=True, indent=intent, separators=separators))
 
 
 def response_schema(data_schema={}):
